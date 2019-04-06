dd4520a5995ccbc01911c9f732f3a3467eec0969
SosthenG <sosthen.gaillard@gmail.com>
2019-04-06 17:35:45 +0200

    Authentication JWT instead of oAuth2

--- lib/HeimdallApi.dart
+++ lib/HeimdallApi.dart
@@ -4,14 +4,12 @@ import "package:http/http.dart" as http;
 
 class HeimdallApi {
   // TODO : Gérer ça via une config à faire au lancement de l'appli, keystore ? ==> https://pub.dartlang.org/packages/flutter_secure_storage
-  static final String clientId = "6_10hitwtgqdesw4k8sc44wgsogcos8840owcso0ok04cwgskkwg";
-  static final String clientSecret = "1wk8ojwd5ts00osos8wssgkcowooowgs84444ocsc444wg0wcw";
-  static final String clientTokenUrl = "http://192.168.1.20/oauth/v2/token";
   static final String clientApiUrl = "http://192.168.1.20/api";
   ApiAccess apiAccess;
 
   Future<bool> login(String username, String password) async {
-    final response = await http.get('$clientTokenUrl?client_id=$clientId&client_secret=$clientSecret&grant_type=password&username=$username&password=$password');
+    Map data = {'username': username, 'password': password};
+    final response = await http.post('$clientApiUrl/login_check', headers: {'Content-Type': 'application/json'}, body: json.encode(data));
 
     if (response.statusCode == 200) { // TODO : Gérer erreurs
       apiAccess = ApiAccess.fromJson(json.decode(response.body));
@@ -22,7 +20,7 @@ class HeimdallApi {
   }
 
   Future<bool> refreshToken() async {
-    final response = await http.get('$clientTokenUrl?client_id=$clientId&client_secret=$clientSecret&grant_type=refresh_token&refresh_token=' + apiAccess.refreshToken);
+    final response = await http.post('$clientApiUrl/token/refresh', body: {'refresh_token': apiAccess.refreshToken});
 
     if (response.statusCode == 200) { // TODO : Gérer erreurs
       apiAccess = ApiAccess.fromJson(json.decode(response.body));
@@ -39,30 +37,20 @@ class HeimdallApi {
 
   Future<http.Response> get(String endpoint, { bool refreshed = false }) async {
     try {
-      final response = await http.get('$clientApiUrl/$endpoint?access_token=' + apiAccess.accessToken);
+      final response = await http.get('$clientApiUrl/$endpoint', headers: {'Authorization': 'Bearer ${apiAccess.token}'});
 
-      if (response.statusCode == 200) {
-        return response;
-      }
-
-      if (refreshed) {
-        // TODO : Déjà refreshed sans succès, on invalide la connexion => Redirection home login
-        print('TODO : déjà refreshed');
-        return null;
-      } else {
-        Map<String, dynamic> json = jsonDecode(response.body);
-        if (json['error'] == 'invalid_grant') {
-          print('INVALID GRANT, trying token refresh...');
-          if (await refreshToken()) {
-            return get(endpoint, refreshed: true);
+      switch (response.statusCode) {
+        case 200:
+          print('Return response');
+          return response;
+        case 401:
+          if (refreshed) {
+            return null; // TODO : Redirection login screen, connexion refusée
           }
-        } else if (json['error'] == 'access_denied') {
-          // TODO : Message access denied
-          print('ACCESS DENIED');
-        } else {
-          print('Error : ${json["error"]} : ${json["error_description"]}');
-        }
+          await refreshToken();
+          return get(endpoint, refreshed: true);
       }
+
     } on Exception catch (e) {
       print(e.toString()); // TODO
     }
@@ -72,18 +60,14 @@ class HeimdallApi {
 }
 
 class ApiAccess {
-  final String accessToken;
-  final int expiresIn;
-  final String scope;
+  final String token;
   final String refreshToken;
 
-  ApiAccess({this.accessToken, this.expiresIn, this.scope, this.refreshToken});
+  ApiAccess({this.token, this.refreshToken});
 
   factory ApiAccess.fromJson(Map<String, dynamic> json) {
     return ApiAccess(
-      accessToken: json['access_token'],
-      expiresIn: json['expires_in'],
-      scope: json['scope'],
+      token: json['token'],
       refreshToken: json['refresh_token'],
     );
   }
